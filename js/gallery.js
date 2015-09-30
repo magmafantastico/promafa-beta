001fafe1fddc7be93feaae4f4915fe60dc9f5c59
dudusbarros <joseeduardobarros@gmail.com>
2015-09-30 18:43:03 -0300

    Add under construction version

---
+++ js/gallery.js
@@ -0,0 +1,6 @@
+var main = document.getElementById('main');
+
+var hero = document.getElementById('hero-gallery');
+var heroSlides = document.querySelectorAll('.gallery-content');
+
+main.addEventListener('click', changeSlide);
